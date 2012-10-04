29a3c24318795b6ff9f54f6b7395cebefb04146b
Dave Abrahams <dave@boostpro.com>
2012-10-04 15:18:51 -0400

    Begin dragging over files from Clang
    Not expected to compile yet
    
    Added a dummy CGDebugInfo to avoid having to make modifications to files just yet

---
+++ lib/ABI/CGDebugInfo.h
@@ -0,0 +1,16 @@
+//===--- CGDebugInfo.h - Emit LLVM Code from ASTs for a Module --------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Dummy; hoping to untangle the one in clang from CodeGenModule
+//
+//===----------------------------------------------------------------------===//
+#ifndef CGDEBUGINFO_DWA2012104_H
+# define CGDEBUGINFO_DWA2012104_H
+
+#endif // CGDEBUGINFO_DWA2012104_H
