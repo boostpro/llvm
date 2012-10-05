b252d3919f1e2a6d8bc0822a279c52d8c2219877
Dave Abrahams <dave@boostpro.com>
2012-10-05 19:10:52 -0400

    Add a few more vocabulary types

--- lib/ABI/ABI.h
+++ lib/ABI/ABI.h
@@ -15,11 +15,28 @@
 #ifndef ABI_DWA2012105_H
 # define ABI_DWA2012105_H
 
+#include "llvm/Support/DataTypes.h"
+
 // Declarations of value types from clang used by ABI-level codegen
 namespace llvm_abi {
 
+class Qualifiers
+{
+ public:
+    class ObjCLifetime {};
+    bool hasVolatile() const;
+    bool hasRestrict() const;
+};
 class QualType {};
 class Type {};
+class Expr;
+class CharUnits
+{
+ public:
+    typedef int64_t QuantityType;
+    QuantityType getQuantity() const;
+};
+class ASTContext {};
 }
 
 #endif // ABI_DWA2012105_H
